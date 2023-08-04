16ee7bcd984290aa31b26cc942042565f885b205
Omar Ivan Gaxiola Sánchez <136842959+Ogaxiola270781@users.noreply.github.com>
2023-08-04 00:17:34 -0700

    Initial commit

---
+++ miPrincipal/Formulageneral.java
@@ -0,0 +1,14 @@
+package miPrincipal;
+
+public class Formulageneral {
+
+    public double raiz1(double a, double b, double c) {
+        return ((-b+(Math.sqrt((b*b)-4*a*c)))/(2*a));
+    }
+
+    public double raiz2(double a, double b, double c) {
+        return ((-b-(Math.sqrt((b*b)-4*a*c)))/(2*a));
+    }
+    
+  
+}
